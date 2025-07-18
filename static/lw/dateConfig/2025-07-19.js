dbfbfc42973d1643137439533d5bd4a7ebfbe4c0
alecthw <alecthw@163.com>
2025-07-18 22:32:57 +0800

    lw 0718

--- static/lw/dateConfig/2025-07-19.js
+++ static/lw/dateConfig/2025-07-19.js
@@ -29,10 +29,13 @@ const Fk1t = {
         '0-0': { color: '#995dd4', alpha: 0.8, text: 'Fk1t' },
         '0-1': { color: '#995dd4', alpha: 0.8, text: 'Fk1t' },
         '1-0': { color: '#995dd4', alpha: 0.8, text: 'Fk1t' },
+        '2-0': { color: '#995dd4', alpha: 0.8, text: 'Fk1t' },
+        '3-0': { color: '#995dd4', alpha: 0.8, text: 'Fk1t' },
     },
     layer2: {
         '0-1': { color: '#995dd4', alpha: 0.8, text: 'Fk1t' },
         '1-0': { color: '#995dd4', alpha: 0.8, text: 'Fk1t' },
+        '3-0': { color: '#995dd4', alpha: 0.8, text: 'Fk1t' },
     },
 };
 
@@ -54,6 +57,7 @@ const R1s = {
         '0-6': { color: '#cdbc35', alpha: 0.8, text: 'R1s' },
         '0-7': { color: '#cdbc35', alpha: 0.8, text: 'R1s' },
         '0-8': { color: '#cdbc35', alpha: 0.8, text: 'R1s' },
+        '1-7': { color: '#cdbc35', alpha: 0.8, text: 'R1s' },
     },
     layer2: {
         '0-4': { color: '#cdbc35', alpha: 0.8, text: 'R1s' },
@@ -68,77 +72,116 @@ const EnM = {
         '0-9': { color: '#d67b1c', alpha: 0.8, text: 'EnM' },
         '0-10': { color: '#d67b1c', alpha: 0.8, text: 'EnM' },
         '0-11': { color: '#d67b1c', alpha: 0.8, text: 'EnM' },
+        '0-12': { color: '#d67b1c', alpha: 0.8, text: 'EnM' },
+        '1-10': { color: '#d67b1c', alpha: 0.8, text: 'EnM' },
     },
     layer2: {
         '0-8': { color: '#d67b1c', alpha: 0.8, text: 'EnM' },
         '0-9': { color: '#d67b1c', alpha: 0.8, text: 'EnM' },
+        '0-10': { color: '#d67b1c', alpha: 0.8, text: 'EnM' },
+    },
+};
+
+const HzZ = {
+    layer1: {
+        '1-12': { color: '#19a9cb', alpha: 0.8, text: 'HzZ' },
+    },
+    layer2: {
     },
 };
 
 const Omz = {
     layer1: {
+        '11-5': { color: '#62bd32', alpha: 0.8, text: 'Omz' },
+        '11-7': { color: '#62bd32', alpha: 0.8, text: 'Omz' },
         '12-5': { color: '#62bd32', alpha: 0.8, text: 'Omz' },
         '12-6': { color: '#62bd32', alpha: 0.8, text: 'Omz' },
         '12-7': { color: '#62bd32', alpha: 0.8, text: 'Omz' },
+        '12-8': { color: '#62bd32', alpha: 0.8, text: 'Omz' },
+        '12-9': { color: '#62bd32', alpha: 0.8, text: 'Omz' },
     },
     layer2: {
         '11-4': { color: '#62bd32', alpha: 0.8, text: 'Omz' },
+        '11-5': { color: '#62bd32', alpha: 0.8, text: 'Omz' },
         '11-6': { color: '#62bd32', alpha: 0.8, text: 'Omz' },
         '11-7': { color: '#62bd32', alpha: 0.8, text: 'Omz' },
+        '11-8': { color: '#62bd32', alpha: 0.8, text: 'Omz' },
+        '11-9': { color: '#62bd32', alpha: 0.8, text: 'Omz' },
+    },
+};
+
+const ulu = {
+    layer1: {
+        '12-4': { color: '#39f709ff', alpha: 0.8, text: 'ulu' },
+    },
+    layer2: {
     },
 };
 
 const TUPA = {
     layer1: {
         '2-12': { color: '#df697f', alpha: 0.8, text: 'TUPA' },
+        '3-11': { color: '#df697f', alpha: 0.8, text: 'TUPA' },
         '3-12': { color: '#df697f', alpha: 0.8, text: 'TUPA' },
+        '4-11': { color: '#df697f', alpha: 0.8, text: 'TUPA' },
         '4-12': { color: '#df697f', alpha: 0.8, text: 'TUPA' },
+        '5-11': { color: '#df697f', alpha: 0.8, text: 'TUPA' },
         '5-12': { color: '#df697f', alpha: 0.8, text: 'TUPA' },
+        '6-11': { color: '#df697f', alpha: 0.8, text: 'TUPA' },
     },
     layer2: {
         '1-11': { color: '#df697f', alpha: 0.8, text: 'TUPA' },
         '2-11': { color: '#df697f', alpha: 0.8, text: 'TUPA' },
         '3-11': { color: '#df697f', alpha: 0.8, text: 'TUPA' },
+        '4-11': { color: '#df697f', alpha: 0.8, text: 'TUPA' },
         '5-11': { color: '#df697f', alpha: 0.8, text: 'TUPA' },
     },
 };
 
 const Phs = {
     layer1: {
-        '7-12': { color: '#d0658f', alpha: 0.8, text: 'Phs' },
-        '8-12': { color: '#d0658f', alpha: 0.8, text: 'Phs' },
-        '9-12': { color: '#d0658f', alpha: 0.8, text: 'Phs' },
-        '10-12': { color: '#d0658f', alpha: 0.8, text: 'Phs' },
-        '11-12': { color: '#d0658f', alpha: 0.8, text: 'Phs' },
-        '12-12': { color: '#d0658f', alpha: 0.8, text: 'Phs' },
+        '7-11': { color: '#e652d7ff', alpha: 0.8, text: 'Phs' },
+        '7-12': { color: '#e652d7ff', alpha: 0.8, text: 'Phs' },
+        '8-12': { color: '#e652d7ff', alpha: 0.8, text: 'Phs' },
+        '9-12': { color: '#e652d7ff', alpha: 0.8, text: 'Phs' },
+        '10-12': { color: '#e652d7ff', alpha: 0.8, text: 'Phs' },
+        '11-12': { color: '#e652d7ff', alpha: 0.8, text: 'Phs' },
+        '12-12': { color: '#e652d7ff', alpha: 0.8, text: 'Phs' },
     },
     layer2: {
-        '7-11': { color: '#d0658f', alpha: 0.8, text: 'Phs' },
-        '8-11': { color: '#d0658f', alpha: 0.8, text: 'Phs' },
-        '9-11': { color: '#d0658f', alpha: 0.8, text: 'Phs' },
-        '10-11': { color: '#d0658f', alpha: 0.8, text: 'Phs' },
+        '6-11': { color: '#e652d7ff', alpha: 0.8, text: 'Phs' },
+        '7-11': { color: '#e652d7ff', alpha: 0.8, text: 'Phs' },
+        '8-11': { color: '#e652d7ff', alpha: 0.8, text: 'Phs' },
+        '9-11': { color: '#e652d7ff', alpha: 0.8, text: 'Phs' },
+        '10-11': { color: '#e652d7ff', alpha: 0.8, text: 'Phs' },
     },
 };
 
 export default {
     layer1: {
         ...ywk.layer1,
-        ...Fk1t.layer1,
-        ...LnG.layer1,
         ...R1s.layer1,
-        ...EnM.layer1,
         ...Omz.layer1,
         ...TUPA.layer1,
         ...Phs.layer1,
+
+        ...Fk1t.layer1,
+        ...EnM.layer1,
+        ...HzZ.layer1,
+        ...ulu.layer1,
+        ...LnG.layer1,
     },
     layer2: {
         ...ywk.layer2,
-        ...Fk1t.layer2,
-        ...LnG.layer2,
         ...R1s.layer2,
-        ...EnM.layer2,
         ...Omz.layer2,
         ...TUPA.layer2,
         ...Phs.layer2,
+
+        ...Fk1t.layer2,
+        ...EnM.layer2,
+        ...HzZ.layer2,
+        ...ulu.layer2,
+        ...LnG.layer2,
     },
 };
---
+++ static/lw/dateConfig/final.js
@@ -0,0 +1,148 @@
+// Capture Drop
+const ywk = {
+    layer1: {
+        '4-5': { color: '#16beae', alpha: 1, text: 'ywk' },
+        '5-4': { color: '#16beae', alpha: 1, text: 'ywk' },
+        '5-5': { color: '#16beae', alpha: 1, text: 'ywk' },
+        '6-4': { color: '#16beae', alpha: 1, text: 'ywk' },
+        '6-5': { color: '#16beae', alpha: 1, text: 'ywk' },
+        '7-4': { color: '#16beae', alpha: 1, text: 'ywk' },
+        '7-5': { color: '#16beae', alpha: 1, text: 'ywk' },
+        '8-4': { color: '#16beae', alpha: 1, text: 'ywk' },
+    },
+    layer2: {
+        '5-3': { color: '#16beae', alpha: 1, text: 'ywk' },
+        '5-4': { color: '#16beae', alpha: 1, text: 'ywk' },
+        '6-3': { color: '#16beae', alpha: 1, text: 'ywk' },
+        '6-4': { color: '#16beae', alpha: 1, text: 'ywk' },
+        '7-3': { color: '#16beae', alpha: 1, text: 'ywk' },
+        '7-5': { color: '#16beae', alpha: 1, text: 'ywk' },
+
+        '3-3': { color: '#16beae', alpha: 1, text: 'ywk' },
+        '4-4': { color: '#16beae', alpha: 1, text: 'ywk' },
+        '5-5': { color: '#16beae', alpha: 1, text: 'ywk' },
+        '6-5': { color: '#16beae', alpha: 1, text: 'ywk' },
+        '7-4': { color: '#16beae', alpha: 1, text: 'ywk' },
+        '8-3': { color: '#16beae', alpha: 1, text: 'ywk' },
+    },
+};
+
+const R1s = {
+    layer1: {
+        '4-4': { color: '#cdbc35', alpha: 1, text: 'R1s' },
+        '3-5': { color: '#cdbc35', alpha: 1, text: 'R1s' },
+        '3-6': { color: '#cdbc35', alpha: 1, text: 'R1s' },
+        '3-7': { color: '#cdbc35', alpha: 1, text: 'R1s' },
+        '3-8': { color: '#cdbc35', alpha: 1, text: 'R1s' },
+        '3-9': { color: '#cdbc35', alpha: 1, text: 'R1s' },
+        '4-6': { color: '#cdbc35', alpha: 1, text: 'R1s' },
+        '5-6': { color: '#cdbc35', alpha: 1, text: 'R1s' },
+    },
+    layer2: {
+        '4-3': { color: '#cdbc35', alpha: 1, text: 'R1s' },
+        '3-4': { color: '#cdbc35', alpha: 1, text: 'R1s' },
+        '2-5': { color: '#cdbc35', alpha: 1, text: 'R1s' },
+        '2-6': { color: '#cdbc35', alpha: 1, text: 'R1s' },
+        '2-7': { color: '#cdbc35', alpha: 1, text: 'R1s' },
+        '2-8': { color: '#cdbc35', alpha: 1, text: 'R1s' },
+        '3-5': { color: '#cdbc35', alpha: 1, text: 'R1s' },
+        '4-5': { color: '#cdbc35', alpha: 1, text: 'R1s' },
+
+        '2-9': { color: '#cdbc35', alpha: 1, text: 'R1s' },
+    },
+};
+
+const Omz = {
+    layer1: {
+        '7-6': { color: '#62bd32', alpha: 1, text: 'Omz' },
+        '8-5': { color: '#62bd32', alpha: 1, text: 'Omz' },
+        '8-6': { color: '#62bd32', alpha: 1, text: 'Omz' },
+        '9-3': { color: '#62bd32', alpha: 1, text: 'Omz' },
+        '9-4': { color: '#62bd32', alpha: 1, text: 'Omz' },
+        '9-5': { color: '#62bd32', alpha: 1, text: 'Omz' },
+        '9-6': { color: '#62bd32', alpha: 1, text: 'Omz' },
+        '9-7': { color: '#62bd32', alpha: 1, text: 'Omz' },
+    },
+    layer2: {
+        '7-6': { color: '#62bd32', alpha: 1, text: 'Omz' },
+        '8-4': { color: '#62bd32', alpha: 1, text: 'Omz' },
+        '8-5': { color: '#62bd32', alpha: 1, text: 'Omz' },
+        '8-6': { color: '#62bd32', alpha: 1, text: 'Omz' },
+        '9-4': { color: '#62bd32', alpha: 1, text: 'Omz' },
+        '9-5': { color: '#62bd32', alpha: 1, text: 'Omz' },
+        '9-6': { color: '#62bd32', alpha: 1, text: 'Omz' },
+        '9-7': { color: '#62bd32', alpha: 1, text: 'Omz' },
+
+        '9-2': { color: '#62bd32', alpha: 1, text: 'Omz' },
+    },
+};
+
+const TUPA = {
+    layer1: {
+        '4-7': { color: '#df697f', alpha: 1, text: 'TUPA' },
+        '4-8': { color: '#df697f', alpha: 1, text: 'TUPA' },
+        '5-7': { color: '#df697f', alpha: 1, text: 'TUPA' },
+        '5-8': { color: '#df697f', alpha: 1, text: 'TUPA' },
+        '6-7': { color: '#df697f', alpha: 1, text: 'TUPA' },
+        '7-7': { color: '#df697f', alpha: 1, text: 'TUPA' },
+        '7-8': { color: '#df697f', alpha: 1, text: 'TUPA' },
+        '8-8': { color: '#df697f', alpha: 1, text: 'TUPA' },
+    },
+    layer2: {
+        '3-6': { color: '#df697f', alpha: 1, text: 'TUPA' },
+        '3-7': { color: '#df697f', alpha: 1, text: 'TUPA' },
+        '4-6': { color: '#df697f', alpha: 1, text: 'TUPA' },
+        '4-8': { color: '#df697f', alpha: 1, text: 'TUPA' },
+        '5-7': { color: '#df697f', alpha: 1, text: 'TUPA' },
+        '5-8': { color: '#df697f', alpha: 1, text: 'TUPA' },
+
+        '3-8': { color: '#df697f', alpha: 1, text: 'TUPA' },
+        '4-7': { color: '#df697f', alpha: 1, text: 'TUPA' },
+        '5-6': { color: '#df697f', alpha: 1, text: 'TUPA' },
+        '6-6': { color: '#df697f', alpha: 1, text: 'TUPA' },
+        '7-7': { color: '#df697f', alpha: 1, text: 'TUPA' },
+        '8-8': { color: '#df697f', alpha: 1, text: 'TUPA' },
+    },
+};
+
+const Phs = {
+    layer1: {
+        '6-8': { color: '#e652d7ff', alpha: 1, text: 'Phs' },
+        '6-9': { color: '#e652d7ff', alpha: 1, text: 'Phs' },
+        '7-9': { color: '#e652d7ff', alpha: 1, text: 'Phs' },
+        '8-7': { color: '#e652d7ff', alpha: 1, text: 'Phs' },
+        '8-9': { color: '#e652d7ff', alpha: 1, text: 'Phs' },
+        '9-8': { color: '#e652d7ff', alpha: 1, text: 'Phs' },
+        '9-9': { color: '#e652d7ff', alpha: 1, text: 'Phs' },
+        '10-9': { color: '#e652d7ff', alpha: 1, text: 'Phs' },
+    },
+    layer2: {
+        '5-9': { color: '#e652d7ff', alpha: 1, text: 'Phs' },
+        '6-7': { color: '#e652d7ff', alpha: 1, text: 'Phs' },
+        '6-8': { color: '#e652d7ff', alpha: 1, text: 'Phs' },
+        '6-9': { color: '#e652d7ff', alpha: 1, text: 'Phs' },
+        '7-8': { color: '#e652d7ff', alpha: 1, text: 'Phs' },
+        '7-9': { color: '#e652d7ff', alpha: 1, text: 'Phs' },
+        '8-7': { color: '#e652d7ff', alpha: 1, text: 'Phs' },
+        '8-9': { color: '#e652d7ff', alpha: 1, text: 'Phs' },
+
+        '9-9': { color: '#e652d7ff', alpha: 1, text: 'Phs' },
+    },
+};
+
+export default {
+    layer1: {
+        ...ywk.layer1,
+        ...R1s.layer1,
+        ...Omz.layer1,
+        ...TUPA.layer1,
+        ...Phs.layer1,
+    },
+    layer2: {
+        ...ywk.layer2,
+        ...R1s.layer2,
+        ...Omz.layer2,
+        ...TUPA.layer2,
+        ...Phs.layer2,
+    },
+};
