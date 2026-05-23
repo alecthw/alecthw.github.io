288dcd306180eb36f714d0bdcfe1d3196f3cb7da
alecthw <alecthw@163.com>
2026-05-23 18:09:11 +0000

    Deploying to gh-pages from @ alecthw/alecthw.github.io@cd6ba79da2b4d53bcb12b72d195d1538fae87bff 🚀

---
+++ lw/dateConfig/2025-07-14.js
@@ -0,0 +1,7 @@
+export default {
+    layer1: {
+        '5-0': { color: '#00ffc3', alpha: 0.8, text: 'Capture' },
+        '6-0': { color: '#00ffc3', alpha: 0.8, text: 'Capture' },
+    },
+    layer2: {},
+};
